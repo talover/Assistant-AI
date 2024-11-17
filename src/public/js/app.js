1f576707f07aab4f2e26214497d8d8052f5801b0
Patrick Santos <your@email.com>
2024-11-17 03:28:39 +0200

    hero

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,7 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    // -- hero typing --
+    var typed = new Typed('#hero-text-typing', {
+        strings: ['контента'],
+        typeSpeed: 50,
+    });
+});
